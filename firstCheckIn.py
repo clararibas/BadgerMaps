8d44f45cd6d5a900941cc63c7c37008ad2289446
Clara Ribas <claralribas@gmail.com>
2022-05-12 01:54:44 +0200

    Earliest Check In Date

--- firstCheckIn.py
+++ firstCheckIn.py
@@ -1,12 +1,11 @@
 import csv
-from operator import itemgetter
 from datetime import datetime
 
 # Reading the CSV File
 file = open('SampleTestFile.csv', encoding="utf8")
 reader = csv.DictReader(file)
 
-#print dates
+# Making a list with the dates
 dates = []
 for col in reader:
     if col['Last Check-In Date'] == '':
@@ -14,9 +13,13 @@ for col in reader:
     else:
         dates.append(col['Last Check-In Date'])
 
+# Sorting the dates list
 dates.sort(key = lambda date: datetime.strptime(date, '%d/%m/%Y'))
+
+# Locate the user with the earliest date
 file = open('SampleTestFile.csv', encoding="utf8")
 reader = csv.DictReader(file)
+
 for line in reader:
     if line['Last Check-In Date'] == dates[0]:
         print(line['First Name'], line['Last Name'],'has the earliest check in date:', dates[0])
