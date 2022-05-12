74cef03b5147d2bad9f3b73097696f1745fda3a1
Clara Ribas <claralribas@gmail.com>
2022-05-12 02:10:59 +0200

    Jobs Ordered

--- orderJob.py
+++ orderJob.py
@@ -1,12 +1,14 @@
 import csv
 from operator import itemgetter
-from datetime import datetime
 
 # Reading the CSV File
 file = open('SampleTestFile.csv', encoding="utf8")
 reader = csv.DictReader(file)
 
-#print sorted job
+# Print sorted job
 for job in sorted(reader, key=itemgetter('Job')):
-    if job['Company'] == 'Badger Maps':
-        print(job['Job'])
+    if job['Job'] == '':
+        continue
+    else:
+        if job['Company'] == 'Badger Maps':
+            print(job['Job'])
